a442c5b9096b3e3d74683bee200c137f0ba9be0e
Denver Fernandes <sub367@gmail.com>
2016-06-27 00:09:52 +0100

    Add manage projects index

---
+++ resources/views/manage/projects/index.blade.php
@@ -0,0 +1,35 @@
+@extends('manage.layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Manage Projects</div>
+
+                    <div class="panel-body">
+                        @if(count($projects) <= 0)
+                            <p>There are currently no projects. <a href="manage/projects/add">Add</a> one?</p>
+                        @else
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Issues</th>
+                                        <th>&nbsp;</th>
+                                        <th>&nbsp;</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        
+                                    </tr>
+                                </tbody>
+                            </table>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
