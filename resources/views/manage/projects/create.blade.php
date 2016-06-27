999bb0dac237cf1f9352eb973af55779233391ed
Denver Fernandes <sub367@gmail.com>
2016-06-27 01:18:17 +0100

    Change title of panel in create new project view

--- resources/views/manage/projects/create.blade.php
+++ resources/views/manage/projects/create.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Manage Projects</div>
+                    <div class="panel-heading">Create New Project</div>
 
                     <div class="panel-body">
                         @include('common.errors')
