003d6d659f6ca08f6c5aa065a3fd6f6590fa1cb1
Denver Fernandes <sub367@gmail.com>
2016-06-27 01:07:24 +0100

    Add table to show existing issues in manage dashboard

--- resources/views/manage/home.blade.php
+++ resources/views/manage/home.blade.php
@@ -10,7 +10,25 @@
                     <div class="panel-body">
                         @if(count($projects) <= 0)
                             <p>There are currently no projects. <a href="manage/projects/add">Add</a> one?</p>
+                        @else
+                            <table class="table table-striped task-table">
+                                <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Issues</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($projects as $project)
+                                    <tr>
+                                        <td>{{$project->name}}</td>
+                                        <td>{{count(App\Project::find($project->id)->issues)}}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
                         @endif
+
                     </div>
                 </div>
             </div>
