d3e68dea9152c5eeedfc05ccfc9c89540cabf95d
Denver Fernandes <sub367@gmail.com>
2016-07-03 14:45:38 +0100

    Add view for creating tags

---
+++ resources/views/manage/tags/create.blade.php
@@ -0,0 +1,30 @@
+@extends('manage.layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Create New Tag</div>
+
+                    <div class="panel-body">
+                        @include('common.errors')
+
+                        <form action="{{url('manage/tags')}}" method="POST">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input name="tag" type="text" class="form-control" placeholder="Name">
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-btn fa-plus"></i>Add Tag
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
