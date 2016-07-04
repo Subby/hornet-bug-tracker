831238d95887810044d993f00d01e2cf0eae2cee
Denver Fernandes <sub367@gmail.com>
2016-07-04 14:07:43 +0100

    Remove issue id column from tags table

--- database/migrations/2016_06_24_163157_create_tags_table.php
+++ database/migrations/2016_06_24_163157_create_tags_table.php
@@ -16,7 +16,6 @@ class CreateTagsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('colour');
-            $table->integer('issue_id');
             $table->timestamps();
         });
     }
